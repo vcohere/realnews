8cfab58e7081d630fb6244524fc7208b4d62dcdc
vcohere <vcohere@gmail.com>
2015-11-22 17:59:22 +0100

    new version

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-	$('#menu').portamento({
-		gap: 0
+	$('.fa-bars').click(function() {
+		if ($('#panel').hasClass('active')) {
+			$('#content').animate({left: '0px'});
+			$('#panel').animate({left: '-220px'}).removeClass('active');
+		}
+		else {
+			$('#content').animate({left: '220px'});
+			$('#panel').animate({left: '0px'}).addClass('active');
+		}
 	});
-	$('#menu li').click(function() {
-		var select = $(this).html();
 
-		$(this).siblings().removeClass('active');
-		$(this).addClass('active');
-
-		$('.article').show();
-		$('.article').each(function() {
-			if ($(this).find('.tag').html().toLowerCase() != select && select != 'tout')
-				$(this).hide();
-		});
-
-		return false;
-	})
+	$('#panel h2').click(function() {
+		$(this).toggleClass('active');
+	});
 });
